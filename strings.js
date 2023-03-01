124fd10510224da6993b94206a25f2cc6a1b487b
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-03-01 12:30:52 +0500

    Practice on Strings

---
+++ strings.js
@@ -0,0 +1,21 @@
+console.log(''); //empty string
+
+console.log(""); //empty string
+
+console.log('hello there!');
+
+console.log('Hello there!123'); //we can combine strings and numbers
+
+// console.log('hello) //error  because strings doesn't go to new line by itself
+
+// console.log("helllo) //error because strings doesn't go to new line by itself
+
+//single or double quotes are called delimeters because we use them to delimit our strings values from the rest of other values
+
+console.log('It is a lovely day');
+
+// console.log('It's a lovely day'); //error due to apostrophe
+
+//to fix this issue, we need to wrap it with double quotes
+console.log("It's is a lovely day");
+
