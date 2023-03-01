75e71951986e23d4cfc7ac88bed14ac73aee0115
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-03-01 12:31:18 +0500

    Practice on Boolean

---
+++ boolean.js
@@ -0,0 +1,16 @@
+console.log(1<2);
+
+console.log(1>2);
+
+console.log(1 == 2);
+
+var score = 100
+console.log(score);
+
+console.log(100 == "100");
+
+console.log(100 === "100");
+
+console.log(1 != "1");
+
+console.log(1 !== "1");
