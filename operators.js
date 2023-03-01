8268c410faed16702b035a8fd5bede1c6e45aa75
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-03-01 11:01:48 +0500

    Practice on Operators
    Arithmetic, Logical, and Comparison Operators

---
+++ operators.js
@@ -0,0 +1,34 @@
+/*Artihmetic Operations: used to perform arithmetic operations on operators
+    + Addition
+    - Subtraction
+    * Multiplication
+    / Division
+*/
+console.log(2+2); //4
+console.log(1+2+3+4+5); //15
+console.log(20-18); //2
+console.log(2*3);  //6
+console.log(8/1);   //8
+
+
+
+/*Comparison Operators:
+    > Greater than
+    < Less than
+    == Equal to
+    != Not equal to
+*/
+console.log(3>2);   //true
+console.log(2<3);   //true
+console.log(10==10);    //true
+console.log(10 != 2); //true
+
+
+/*
+Logical Operators:
+    && - AND - returuns true when both sides are true
+    || - OR - returen true when atleast one side is true
+    !  - NOT - reverse result 
+*/
+console.log((1>2) && (1==1)); //false
+console.log((1>2) || (1==1));   //true
